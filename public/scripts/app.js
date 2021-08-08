0d7ffa2cda78bdd84a83ee4df0b6da1f6907eb5a
cedafiso <cedafiso@gmail.com>
2021-08-07 20:29:18 -0500

    Starting

---
+++ public/scripts/app.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var template = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "p",
+    null,
+    "Funciona!"
+  )
+);
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,4 @@
+var template = <div><p>Funciona!</p></div>;
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
