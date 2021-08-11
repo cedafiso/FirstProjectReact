a2948b78db8b997febe53b906f2cc67dd0283f7d
cedafiso <cedafiso@gmail.com>
2021-08-11 14:56:35 -0500

    Webpack configurado

--- public/scripts/app.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-
-console.log("Me la pelas");
--- src/pruebas/app.js
+++ src/pruebas/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react';
+import ReactDOM from 'react-dom';
 class Reto3 extends React.Component{
     constructor(props){
         super(props);
