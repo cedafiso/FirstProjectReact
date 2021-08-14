584207d796971fd61491980f1111f654309f8469
cedafiso <cedafiso@gmail.com>
2021-08-14 14:42:09 -0500

    Configuration

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Reto3 from './componentes/Reto3';
 import './estilos/base/_setting.scss';
 import '../node_modules/normalize.css/normalize.css'
 import './estilos/estilo.scss';
 
-ReactDOM.render(<Reto3 />, document.getElementById('app'))
+ReactDOM.render(<p>Boiler Plate</p>, document.getElementById('app'))
